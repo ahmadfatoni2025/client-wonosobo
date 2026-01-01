c7bf17aaf76b3d7bf8efd69ace7bd5627b001e46
Ahmad fatoni <ahfatoni2026@gmail.com>
2026-01-01 23:14:11 +0700

    login google auth

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -2,15 +2,10 @@
 
 import * as React from "react"
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
 import {
-  AudioWaveform,
-  BookOpen,
   Bot,
-  Command,
   Frame,
   GalleryVerticalEnd,
-  Map,
   PieChart,
   Settings2,
   SquareTerminal,
